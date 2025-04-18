cc7e193e815692cdc0c76af78d57a6479a97f58d
Robert Baranic <168925727+bobbybar@users.noreply.github.com>
2025-04-18 13:13:33 -0700

    Create 1280.sql

---
+++ easy/1280.sql
@@ -0,0 +1,43 @@
+Not "easy"
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| student_id    | int     |
+| student_name  | varchar |
++---------------+---------+
+student_id is the primary key (column with unique values) for this table.
+Each row of this table contains the ID and the name of one student in the school.
+ 
+
+Table: Subjects
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| subject_name | varchar |
++--------------+---------+
+subject_name is the primary key (column with unique values) for this table.
+Each row of this table contains the name of one subject in the school.
+ 
+
+Table: Examinations
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| student_id   | int     |
+| subject_name | varchar |
++--------------+---------+
+There is no primary key (column with unique values) for this table. It may contain duplicates.
+Each student from the Students table takes every course from the Subjects table.
+Each row of this table indicates that a student with ID student_id attended the exam of subject_name.
+ 
+
+Write a solution to find the number of times each student attended each exam.
+
+Return the result table ordered by student_id and subject_name.
+
+SELECT a.student_id, a.student_name, b.subject_name, COUNT(c.subject_name) as attended_exams
+FROM Students a CROSS JOIN Subjects b LEFT JOIN Examinations c ON (a.student_id = c.student_id AND b.subject_name = c.subject_name)
+GROUP BY a.student_id, a.student_name, b.subject_name
+ORDER BY a.student_id, b.subject_name;
