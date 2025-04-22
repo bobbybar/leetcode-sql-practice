b6c985a5c53896ee3f5108606eb2d2510be13ad4
Robert Baranic <168925727+bobbybar@users.noreply.github.com>
2025-04-22 14:26:29 -0700

    Create 1633.sql

---
+++ easy/1633.sql
@@ -0,0 +1,30 @@
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| user_id     | int     |
+| user_name   | varchar |
++-------------+---------+
+user_id is the primary key (column with unique values) for this table.
+Each row of this table contains the name and the id of a user.
+ 
+
+Table: Register
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| contest_id  | int     |
+| user_id     | int     |
++-------------+---------+
+(contest_id, user_id) is the primary key (combination of columns with unique values) for this table.
+Each row of this table contains the id of a user and the contest they registered into.
+ 
+
+Write a solution to find the percentage of the users registered in each contest rounded to two decimals.
+
+Return the result table ordered by percentage in descending order. In case of a tie, order it by contest_id in ascending order.
+
+SELECT contest_id, ROUND(COUNT(a.user_id) / (SELECT COUNT(DISTINCT user_id) FROM Users) * 100, 2)as percentage
+FROM Users a JOIN Register b ON (a.user_id = b.user_id)
+GROUP BY contest_id
+ORDER BY percentage DESC, contest_id ASC;
