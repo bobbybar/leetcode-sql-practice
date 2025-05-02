8f23e4042e4c4075fa532e261f25e251c59e4799
Robert Baranic <168925727+bobbybar@users.noreply.github.com>
2025-05-02 14:41:47 -0700

    Create 1070.sql

---
+++ medium/1070.sql
@@ -0,0 +1,36 @@
++-------------+-------+
+| Column Name | Type  |
++-------------+-------+
+| sale_id     | int   |
+| product_id  | int   |
+| year        | int   |
+| quantity    | int   |
+| price       | int   |
++-------------+-------+
+(sale_id, year) is the primary key (combination of columns with unique values) of this table.
+product_id is a foreign key (reference column) to Product table.
+Each row of this table shows a sale on the product product_id in a certain year.
+Note that the price is per unit.
+
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
++--------------+---------+
+product_id is the primary key (column with unique values) of this table.
+Each row of this table indicates the product name of each product.
+
+Write a solution to select the product id, year, quantity, and price for the first year of every product sold. If any product is bought multiple times in its first year, return all sales separately.
+
+Return the resulting table in any order.
+
+SELECT product_id, year as first_year, quantity,price
+FROM Sales
+WHERE (product_id,year) in (
+SELECT product_id,MIN(year)
+FROM Sales
+GROUP BY product_id
+)
