cc894db438de44eb601b89bab7eb38e4a5487df5
Robert Baranic <168925727+bobbybar@users.noreply.github.com>
2025-06-24 13:04:15 -0700

    Create 185.sql

---
+++ hard/185.sql
@@ -0,0 +1,36 @@
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| id           | int     |
+| name         | varchar |
+| salary       | int     |
+| departmentId | int     |
++--------------+---------+
+id is the primary key (column with unique values) for this table.
+departmentId is a foreign key (reference column) of the ID from the Department table.
+Each row of this table indicates the ID, name, and salary of an employee. It also contains the ID of their department.
+ 
+
+Table: Department
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the ID of a department and its name.
+ 
+
+A company's executives are interested in seeing who earns the most money in each of the company's departments. A high earner in a department is an employee who has a salary in the top three unique salaries for that department.
+
+Write a solution to find the employees who are high earners in each of the departments.
+
+WITH salary_rank AS (
+    SELECT *, DENSE_RANK() OVER (PARTITION BY departmentId ORDER BY salary DESC) AS rnk
+    FROM Employee
+)
+SELECT b.name AS Department,  a.name as Employee, salary AS Salary
+FROM salary_rank a JOIN Department b ON (a.departmentId = b.id)
+WHERE rnk BETWEEN 1 AND 3;
