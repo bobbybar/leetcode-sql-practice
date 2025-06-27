c35c2af889bc1262469b88f6434308271fa7034a
Robert Baranic <168925727+bobbybar@users.noreply.github.com>
2025-06-27 14:00:15 -0700

    Create 184.sql

---
+++ medium/184.sql
@@ -0,0 +1,35 @@
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| id           | int     |
+| name         | varchar |
+| salary       | int     |
+| departmentId | int     |
++--------------+---------+
+id is the primary key (column with unique values) for this table.
+departmentId is a foreign key (reference columns) of the ID from the Department table.
+Each row of this table indicates the ID, name, and salary of an employee. It also contains the ID of their department.
+ 
+
+Table: Department
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table. It is guaranteed that department name is not NULL.
+Each row of this table indicates the ID of a department and its name.
+ 
+
+Write a solution to find employees who have the highest salary in each of the departments.
+
+WITH cte AS (
+    SELECT b.name as Department, a.name as Employee, a.salary as Salary, 
+        RANK() OVER (PARTITION BY b.id ORDER BY salary DESC) as rn
+    FROM Employee a JOIN Department b ON (a.departmentId = b.id)
+)
+SELECT Department, Employee, Salary
+FROM cte
+WHERE rn = 1;
